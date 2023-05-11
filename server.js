ba034d3ff73ba8e00656d1a1b8c123ea8e9dae4c
Foursharks <69390429+Foursharks@users.noreply.github.com>
2023-05-11 15:36:03 -0400

    Added endpoint to test Rollbar

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ var rollbar = new Rollbar({
   captureUnhandledRejections: true,
 })
 
+const dadBtn = document.getElementById("dadButton"); 
+const getDadJoke = () => axios.get("https://icanhazdadjoke.com/",{
+    headers: {
+      'Accept': 'application/json'
+    }}
+    ).then(dadCallback).catch(err => {console.log(err)})
+
+//Add an event listener to the button so that when clicked the clicked function runs
+dadBtn.addEventListener(`click`, getDadJoke);
+
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
