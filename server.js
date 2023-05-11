bd63bda67bd7ec50650a34a580d79b117d221a94
Foursharks <69390429+Foursharks@users.noreply.github.com>
2023-05-11 15:20:17 -0400

    Added rollbar

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 app.use(express.static(`${__dirname}/public`))
+require('dotenv').config() 
+const {ROLLBAR_ACCESS_TOKEN} = process.env
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: `${ROLLBAR_ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 app.listen(4000, () => console.log(`server running on 4000`))
 
 
